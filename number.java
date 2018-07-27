8c36ea605d32cca7d90d08cc253754317e9dc58b
narendrabeeram <41769627+narendrabeeram@users.noreply.github.com>
2018-07-27 10:01:50 +0530

    Create number.java

---
+++ number.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+				int a=3;
+				if(a>0)
+					System.out.println("given number is postive");
+				else if(a<0)
+					System.out.println("given number is negitive");
+				else
+					System.out.println("given number is zero");
+ 
+	}
+}
