4d8f1e8f64c50d674da628e44b979ce31d47abee
narendrabeeram <41769627+narendrabeeram@users.noreply.github.com>
2018-07-28 11:58:46 +0530

    Update alphabet1.java

--- alphabet1.java
+++ alphabet1.java
@@ -8,7 +8,7 @@ class Ideone
 	public static void main (String[] args) throws java.lang.Exception
 	{
 	Scanner s=new Scanner(System.in);
-	 char ch=s.next.charAt(0);
+	 char ch=s.next().charAt(0);
 	if(ch>='a'&&ch<='z')
 	 {
 	 	System.out.println("Alphabet");
