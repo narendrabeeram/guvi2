791e3286ff0aaa0c53f9dbb00b7ae44be9003090
narendrabeeram <41769627+narendrabeeram@users.noreply.github.com>
2018-08-13 20:37:59 +0530

    Create 49.java

---
+++ 49.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int x,y,z;
+		Scanner s=new Scanner(System.in);
+		x=s.nextInt();
+		y=s.nextInt();
+		z=x+y;
+		System.out.println(z);
+	}
+}
